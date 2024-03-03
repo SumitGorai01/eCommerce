69cd0061c143ebb568e35d870aa0a7d2cf6fcc64
Sumit <goraisumit01@gmail.com>
2024-03-03 20:29:55 +0530

    work on sign in

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,7 +5,17 @@
  */
 
 const authController = require("../controllers/auth.controller")
-
+/**
+ * POST http://localhost/ecomm/api/v1/auth/signup
+ * routes for singUp
+ */
 module.exports = (app)=>{
     app.post("/ecomm/api/v1/auth/signup",authController.signup)
+}
+/**
+ * POST http://localhost/ecomm/api/v1/auth/signin
+ * routes for sign in
+ */
+module.exports = (app)=>{
+    app.post("/ecomm/api/v1/auth/signin",authController.signin)
 }
